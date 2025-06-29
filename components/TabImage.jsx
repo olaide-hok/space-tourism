561edf00b43a6fb0aa9144146f85af0f60fea5c9
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-06-29 11:20:32 +0100

    conditionally render tab images using tech boolean prop

--- components/TabImage.jsx
+++ components/TabImage.jsx
@@ -1,12 +1,37 @@
-export default function TabImage({tab, path, isVisible}) {
-    return (
-        <picture id={tab.imageId} hidden={!isVisible}>
-            <source srcSet={tab.images.webp} type="image/webp" />
-            <img
-                src={tab.images.png}
-                alt={`Illustration for ${tab.label}`}
-                className="w-full max-w-md"
-            />
-        </picture>
-    );
+export default function TabImage({tab, tech, isVisible}) {
+    if (!tech) {
+        return (
+            <picture id={tab.imageId} hidden={!isVisible}>
+                <source srcSet={tab.images.webp} type="image/webp" />
+                <img
+                    src={tab.images.png}
+                    alt={`Illustration for ${tab.label}`}
+                    className="w-full max-w-md"
+                />
+            </picture>
+        );
+    }
+
+    if (tech) {
+        return (
+            <picture id={tab.imageId} hidden={!isVisible}>
+                <source
+                    srcSet={tab.images.portrait}
+                    media="(min-width: 720px)"
+                    type="image/jpg"
+                />
+                <source
+                    srcSet={tab.images.landscape}
+                    media="(min-width: 560px)"
+                    type="image/jpg"
+                />
+
+                <img
+                    src={tab.images.landscape}
+                    alt={`Illustration for ${tab.name}`}
+                    className="w-full max-w-md"
+                />
+            </picture>
+        );
+    }
 }
