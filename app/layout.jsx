b93f596533dfa9713241abbac54b01d217acdb39
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-06-29 11:18:49 +0100

    set className based on pathname

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,24 @@
+'use client';
 import '@/app/ui/index.css';
 import Navigation from '@/components/Navigation';
+import {usePathname} from 'next/navigation';
 
 export default function RootLayout({children}) {
+    const pathname = usePathname();
+    const classToUse =
+        pathname === '/'
+            ? 'home'
+            : pathname === '/destination'
+            ? 'destination'
+            : pathname === '/crew'
+            ? 'crew'
+            : pathname === '/technology'
+            ? 'technology'
+            : '';
+
     return (
         <html lang="en">
-            <body className="home">
+            <body className={classToUse}>
                 <Navigation />
                 {children}
             </body>
