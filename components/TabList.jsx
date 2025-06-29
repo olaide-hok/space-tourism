a26d7c00dd8aa1981a9afe22cda0449437dfe67f
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-06-29 11:45:19 +0100

    conditionally render TabList based on dot, text, and number props.

--- components/TabList.jsx
+++ components/TabList.jsx
@@ -6,23 +6,71 @@ export default function TabList({
     setSelectedTab,
     tabListRef,
     tabsRef,
+    number,
+    text,
+    dot,
 }) {
-    return (
-        <div
-            role="tablist"
-            aria-label="destination list"
-            ref={tabListRef}
-            className="tab-list underline-indicators flex">
-            {tabs.map((tab, index) => (
-                <Tab
-                    key={tab.id}
-                    tab={tab}
-                    index={index}
-                    isSelected={selectedTab === index}
-                    onClick={() => setSelectedTab(index)}
-                    refEl={(el) => (tabsRef.current[index] = el)}
-                />
-            ))}
-        </div>
-    );
+    if (text) {
+        return (
+            <div
+                role="tablist"
+                aria-label="destination list"
+                ref={tabListRef}
+                className="tab-list underline-indicators flex">
+                {tabs.map((tab, index) => (
+                    <Tab
+                        text
+                        key={tab.id}
+                        tab={tab}
+                        index={index}
+                        isSelected={selectedTab === index}
+                        onClick={() => setSelectedTab(index)}
+                        refEl={(el) => (tabsRef.current[index] = el)}
+                    />
+                ))}
+            </div>
+        );
+    }
+    if (dot) {
+        return (
+            <div
+                role="tablist"
+                aria-label="crew member list"
+                ref={tabListRef}
+                class="dot-indicators flex">
+                {tabs.map((tab, index) => (
+                    <Tab
+                        dot
+                        key={tab.id}
+                        tab={tab}
+                        index={index}
+                        isSelected={selectedTab === index}
+                        onClick={() => setSelectedTab(index)}
+                        refEl={(el) => (tabsRef.current[index] = el)}
+                    />
+                ))}
+            </div>
+        );
+    }
+    if (number) {
+        return (
+            <div
+                role="tablist"
+                aria-label="technology list"
+                ref={tabListRef}
+                className="number-indicators flex">
+                {tabs.map((tab, index) => (
+                    <Tab
+                        number
+                        key={tab.id}
+                        tab={tab}
+                        index={index}
+                        isSelected={selectedTab === index}
+                        onClick={() => setSelectedTab(index)}
+                        refEl={(el) => (tabsRef.current[index] = el)}
+                    />
+                ))}
+            </div>
+        );
+    }
 }
