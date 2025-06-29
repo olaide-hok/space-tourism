1a1a04e6e7290a3335886daa6a7503aedd495b5d
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-06-29 11:41:59 +0100

    conditionally render Tab based on text, dot , and number props.

--- components/Tab.jsx
+++ components/Tab.jsx
@@ -1,15 +1,57 @@
-export default function Tab({tab, index, isSelected, onClick, refEl}) {
-    return (
-        <button
-            role="tab"
-            aria-selected={isSelected}
-            aria-controls={tab.id}
-            data-image={tab.imageId}
-            tabIndex={isSelected ? 0 : -1}
-            onClick={onClick}
-            ref={refEl}
-            className="uppercase ff-sans-cond text-accent letter-spacing-2">
-            {tab.name}
-        </button>
-    );
+export default function Tab({
+    tab,
+    text,
+    dot,
+    number,
+    index,
+    isSelected,
+    onClick,
+    refEl,
+}) {
+    if (text) {
+        return (
+            <button
+                role="tab"
+                aria-selected={isSelected}
+                aria-controls={tab.id}
+                data-image={tab.imageId}
+                tabIndex={isSelected ? 0 : -1}
+                onClick={onClick}
+                ref={refEl}
+                className="uppercase ff-sans-cond text-accent letter-spacing-2">
+                {tab.name}
+            </button>
+        );
+    }
+
+    if (dot) {
+        return (
+            <button
+                role="tab"
+                aria-selected={isSelected}
+                aria-controls={tab.id}
+                data-image={tab.imageId}
+                tabIndex={isSelected ? 0 : -1}
+                onClick={onClick}
+                ref={refEl}>
+                <span className="sr-only"> {tab.name}</span>
+            </button>
+        );
+    }
+
+    if (number) {
+        return (
+            <button
+                role="tab"
+                aria-selected={isSelected}
+                aria-controls={tab.id}
+                data-image={tab.imageId}
+                tabIndex={isSelected ? 0 : -1}
+                onClick={onClick}
+                ref={refEl}
+                className="ff-serif text-white">
+                {index + 1}
+            </button>
+        );
+    }
 }
